dc7a60544ef8489b22d06d2ef725217ee16eac59
ALCLynch <alexlynch83@gmail.com>
2019-05-30 13:23:54 -0700

    Board prints and looks decent

--- tictactoe.py
+++ tictactoe.py
@@ -5,8 +5,10 @@
 #       After game ends, enter 'N' for new game, 'Q' for quit
 #       Score is kept track
 #           X - 5, O - 2, Tie - 52
-
-win_conditions = [(1, 2, 3),
+def main():
+    board = ['-' for i in range(1,10)]
+    print_board(board)
+    win_conditions = [(1, 2, 3),
                  (4, 5, 6),
                  (7, 8, 9),
                  (1, 4, 7),
@@ -14,32 +16,19 @@ win_conditions = [(1, 2, 3),
                  (3, 6, 9),
                  (1, 5, 9),
                  (3, 5, 7)]
-
+    
 def print_board(board):
-    print('   |    |  ')
-    print('{} | {} | {}'.format(board[1:3]))
-    print('   |    |')
-    print( '-----------' )
-    print('   |    |')
-    print('{} | {} | {}' .format(board[4:6]))
-    print(   '|    |')
-    print( '-----------')
-    print(   '|   |')
-    print(  '{} | {} | {}'.format(board[7:9]))
-    print(  '|   |'
-
-
-
-def new_game():
-    print('Would you like another game? Type Yes or No:')
-    return input.startswith('y')
-
-def tic_tac_toe():
-    board = ['' for  in range(10)]
+    print('       |       |  ')
+    print('   {}   |   {}   |   {}'.format(board[0],board[1],board[2]))
+    print('       |       |')
+    print('-----------------------' )
+    print('       |       |')
+    print('   {}   |   {}   |   {}'.format(board[3],board[4],board[5]))
+    print('       |       |')
+    print('-----------------------')
+    print('       |       |')
+    print('   {}   |   {}   |   {}'.format(board[6],board[7],board[8]))
+    print('       |       |')
 
 if __name__ == '__main__':
-    print('Tic Tac Toe')
-    while True:
-        ttt()
-        if not new_game():
-               
+    main()  
